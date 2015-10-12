ba222ff83d9f3314e46f8a17626e6a4724e91fc4
Charles Anthony Browne <charlesanthonybrowne@Snoopy-MacBook-Pro.local>
2015-10-12 12:10:03 +0100

    gulpfile and stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var
 	gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
+  vars   = require('postcss-simple-vars')
 	processors = [
 		require('postcss-mixins'),
 		require('postcss-simple-vars'),
@@ -9,8 +10,23 @@ var
 	];
 
   // compile CSS
-  gulp.task('css', function() {
-    return gulp.src('source/css/styles.css')
-      .pipe(postcss(processors))
-      .pipe(gulp.dest('dest/styles/'));
+  gulp.task('css', function () {
+     return gulp.src('./postcss/**/*.css')
+        // .pipe(postcss([ vars({ variables: colors }) ]))
+        .pipe(postcss([ require('postcss-simple-vars')({ silent: true }) ]))
+        .pipe(gulp.dest('./bundle/stylescss/main.css'));
+});
+
+  // gulp.task('css', function() {
+  //   return gulp.src('postcss/**/*.css')
+  //     .pipe(postcss(processors))
+  //     .pipe(gulp.dest('./bundle/stylescss/'));
+  // });
+
+  gulp.task('sass', function() {
+      gulp.src('sass/**/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./bundle/stylescss'));
   });
+
+gulp.task('default', ["css", 'sass']);
