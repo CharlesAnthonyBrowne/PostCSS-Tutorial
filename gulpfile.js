8c9b5b927bf389a63dcc7c4ca5a525caf06f4ab4
Charles Anthony Browne <charlesanthonybrowne@Snoopy-MacBook-Pro.local>
2015-10-12 12:59:21 +0100

    working postcss compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
 var
-	gulp = require('gulp'),
-	postcss = require('gulp-postcss'),
-  vars   = require('postcss-simple-vars')
-  concat = require('gulp-concat');
-	autoprefixer = require('autoprefixer');
-	processors = [
-		require('postcss-mixins'),
-		require('postcss-simple-vars'),
-		require('postcss-nested'),
-		require('autoprefixer')({ browsers: ['last 2 versions', '> 2%'] })
+	gulp           = require('gulp'),
+	postcss        = require('gulp-postcss'),
+  vars           = require('postcss-simple-vars')
+	postcss        = require('gulp-postcss');
+  concat         = require('gulp-concat');
+	fs             = require("fs")
+	url            = require("postcss-url")
+	sourcemaps     = require('gulp-sourcemaps');
+	autoprefixer   = require('autoprefixer');
+	atImport       = require("postcss-import")
+	processors     = [
+									 require('postcss-mixins'),
+									 require('postcss-simple-vars'),
+									 require('postcss-nested'),
+									 require('autoprefixer')({ browsers: ['last 2 versions', '> 2%'] })
 	];
 
 // Watch task
-gulp.task('watch', function(){
-  gulp.watch('./postcss/**/*.css', ['css']);
-});
+// gulp.task('watch', function(){
+//   gulp.watch('./postcss/**/*.css', ['css']);
+// });
 
-// compile CSS
 gulp.task('css', function () {
-   return gulp.src('./postcss/**/*.css')
-      // .pipe(postcss([ vars({ variables: colors }) ]))
-      .pipe(postcss(processors))
-      .pipe(gulp.dest('./bundle/stylescss/'));
+    return gulp.src('./postcss/*.css')
+        .pipe(sourcemaps.init())
+	      .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./bundle/stylescss/'));
 });
 
-gulp.task('default', ["css", 'watch']);
+
+gulp.task('default', ["css"]);
